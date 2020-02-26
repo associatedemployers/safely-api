c8df0fb590577b5b4657f99334a06ebd3fac131c
James <james@aehr.org>
2020-02-26 15:53:53 -0700

    Aggregation work, geocoding, etc.

---
+++ api/hub-classes/index.js
@@ -0,0 +1,77 @@
+exports.withAvailability = async function (n, HubClass, compiledQuery) {
+  const {
+    query,
+    select,
+    sort,
+    limit
+  } = compiledQuery;
+
+  let $project = {
+    registrations: 0
+  };
+
+  if (select && select.length) {
+    $project = select.split(' ').reduce((projectionObj, selectKey) => ({
+      ...projectionObj,
+      [selectKey.replace('-', '')]: selectKey.charAt(0) === '-' ? 0 : 1
+    }), { ...$project });
+  }
+
+  let optionalStages = [];
+
+  if (sort) {
+    optionalStages.push({
+      $sort: {
+        ...sort || {}
+      }
+    });
+  }
+
+  if (limit) {
+    optionalStages.push({
+      $limit: limit
+    });
+  }
+
+  const hubClass = await HubClass.aggregate([{
+    $match: query || {}
+  },
+  ...optionalStages,
+  {
+    $lookup: {
+      from: 'hubregistrations',
+      let: { hubClass: '$_id' },
+      as: 'registrations',
+      pipeline: [{
+        $match: {
+          $expr: { $eq: [ '$hubClass', '$$hubClass' ] }
+        }
+      }, {
+        $project: {
+          participantCount: { $sum: { $size: '$participants' } }
+        }
+      }, {
+        $group: {
+          _id: null,
+          totalParticipants: { $sum: '$participantCount' }
+        }
+      }]
+    }
+  }, {
+    $replaceRoot: {
+      newRoot: {
+        $mergeObjects: [{ $arrayElemAt: [ '$registrations', 0 ] }, '$$ROOT' ]
+      }
+    }
+  }, {
+    $addFields: {
+      seatsRemaining: { $subtract: [ '$seats', '$totalParticipants' ]}
+    }
+  }, {
+    $project
+  }]);
+
+  return {
+    hubClass
+  };
+};
