8824db272fce0fa630613b6a607d5638cf6b6278
James <james@aehr.org>
2020-02-28 15:02:34 -0700

    Hub retrieval

--- api/hub-company/index.js
+++ api/hub-company/index.js
@@ -1,3 +1,20 @@
+const HubCompany = require('../../lib/models/hub-company');
+
 exports.getHubCompany = function* () {
-  
+  const email = (this.request.body.email || '').toLowerCase();
+
+  if (!email || !email.length) {
+    this.status = 400;
+    return;
+  }
+
+  const memberStatus = yield HubCompany.searchForMember(email);
+
+  this.status = 200;
+  this.body = {
+    memberStatus,
+    existingCompany: yield HubCompany.findOne({
+      email
+    })
+  };
 };
