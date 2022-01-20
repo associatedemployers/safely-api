719d0e93d50158398617a1fb944feeb8cdd9bb72
Jeremy Cherer <chererjeremy@gmail.com>
2022-01-20 14:07:28 -0700

    working query

--- api/hub-classes/index.js
+++ api/hub-classes/index.js
@@ -5,6 +5,88 @@ exports.withAvailability = async function (n, HubClass, compiledQuery) {
     sort,
     limit
   } = compiledQuery;
+
+  let $project = {
+    registrations: 0,
+    rosterAccessKey: 0
+  };
+
+  if (select && select.length) {
+    $project = select.split(' ').reduce((projectionObj, selectKey) => ({
+      ...projectionObj,
+      [selectKey.replace('-', '')]: selectKey.charAt(0) === '-' ? 0 : 1
+    }), { ...$project });
+  }
+
+  let optionalStages = [];
+
+  if (sort) {
+    optionalStages.push({
+      $sort: {
+        ...sort || {}
+      }
+    });
+  }
+
+  if (limit) {
+    optionalStages.push({
+      $limit: limit
+    });
+  }
+
+  const hubClass = await HubClass.aggregate([{
+    $match: {
+      ...query || {},
+      'times.0.start': { $gte: new Date() }
+    }
+  },
+  ...optionalStages,
+  {
+    $lookup: {
+      from: 'hubregistrations',
+      let: { hubClass: '$_id' },
+      as: 'registrations',
+      pipeline: [{
+        $match: {
+          $expr: { $eq: [ '$hubClass', '$$hubClass' ] }
+        }
+      }, {
+        $project: {
+          participantCount: { $sum: { $size: '$participants' } }
+        }
+      }, {
+        $group: {
+          _id: null,
+          totalParticipants: { $sum: '$participantCount' }
+        }
+      }]
+    }
+  }, {
+    $replaceRoot: {
+      newRoot: {
+        $mergeObjects: [{ $arrayElemAt: [ '$registrations', 0 ] }, '$$ROOT' ]
+      }
+    }
+  }, {
+    $addFields: {
+      seatsRemaining: { $cond: [ '$totalParticipants', { $subtract: [ '$seats', '$totalParticipants' ] }, '$seats' ] }
+    }
+  }, {
+    $project
+  }]);
+
+  return {
+    hubClass
+  };
+};
+
+exports.bookedResources = async function (n, HubClass, compiledQuery) {
+  const {
+    query,
+    select,
+    sort,
+    limit
+  } = compiledQuery;
   const moment = require('moment');
 
 
