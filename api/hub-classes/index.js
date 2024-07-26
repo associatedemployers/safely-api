07a18c15287822dc462af6899c8e0e26a946196e
Scott McKay <evilscaught@gmail.com>
2024-07-26 13:30:36 -0600

    Add getGeneratedTimes to hub-class API

--- api/hub-classes/index.js
+++ api/hub-classes/index.js
@@ -1,4 +1,8 @@
 exports.withAvailability = async function (n, HubClass, compiledQuery) {
+  const moment = require('moment');
+  const { recur } = require('moment-recur');
+  const winston = require('winston');
+
   const {
     query,
     select,
@@ -74,7 +78,43 @@ exports.withAvailability = async function (n, HubClass, compiledQuery) {
     $project
   }]);
 
+  function getGeneratedTimes (cl) {
+    const REPEATER = 8;
+
+    if (!((cl || {}).generateTimeRef || {}).dayRef) {
+      winston.debug('generateTimes and generateTimeRef must be set before calling generatedTimes');
+      return [];
+    }
+
+    let nextDates = [];
+    let { dayRef, timeBlock } = cl.generateTimeRef;
+    if (dayRef) {
+      let today = moment().startOf('day');
+      let ref = recur(today).every(dayRef).daysOfWeek();
+
+      // Front-End Registrations Code Example
+      // Do not create a timeblock if there hub-class is full
+      nextDates = ref.next(REPEATER).reduce((acc, cur, index) => {
+        let startString = `${moment(cur).format('MM DD YYYY')} ${timeBlock.start.hour}:${timeBlock.start.min}`;
+        let endString = `${moment(cur).format('MM DD YYYY')} ${timeBlock.end.hour}:${timeBlock.end.min}`;
+
+        acc.push({
+          start: new Date(startString).toLocaleString('en', { timeZone: 'America/Denver' }),
+          end:   new Date(endString).toLocaleString('en', { timeZone: 'America/Denver' }),
+          _id:   index
+        });
+        return acc;
+      }, []);
+    }
+    return nextDates;
+  }
+
+  const hubClasses = hubClass.map(hbClass => {
+    hbClass.times.push(...getGeneratedTimes(hbClass));
+    return hbClass;
+  });
+ 
   return {
-    hubClass
+    hubClass: hubClasses
   };
 };
